8fd975d52d3548544977b0bdeab2c52a12fa15bb
Cory Reed <swashcap@gmail.com>
2019-08-23 23:13:09 -0700

    feat: scaffold out apollo-server

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import 'hard-rejection/register'
+
+import fs from 'fs'
+import path from 'path'
+import { ApolloServer, gql } from 'apollo-server'
+
+export const server = new ApolloServer({
+  resolvers: {},
+  typeDefs: gql`${fs.readFileSync(path.join(__dirname, 'schema.gql'))}`
+})
+
+if (require.main === module) {
+  ;(async () => {
+    const { url } = await server.listen()
+    console.log(`Server ready at ${url}`)
+  })()
+}
