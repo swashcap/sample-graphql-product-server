00b771b26d47fdcbde390ff21d685d900eae1bf0
Cory Reed <swashcap@gmail.com>
2019-08-24 11:47:11 -0700

    refactor: paginate product items with `ProductPage` type

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,32 @@ import 'hard-rejection/register'
 
 import fs from 'fs'
 import path from 'path'
-import { ApolloServer, gql } from 'apollo-server'
+import { ApolloServer, gql, IFieldResolver } from 'apollo-server'
+
+const products: IFieldResolver<any, any, GQL.IProductsOnQueryArguments> = (
+  source,
+  args
+): GQL.IQuery['products'] => {
+  let { offset, limit } = args
+
+  offset = typeof offset !== 'number' ? 0 : offset
+  limit = typeof limit !== 'number' ? 20 : limit
+
+  return {
+    __typename: 'ProductsPage',
+    items: [],
+    limit,
+    offset,
+    totalProductsCount: 100
+  }
+}
 
 export const server = new ApolloServer({
   cors: true,
   playground: true,
   resolvers: {
     Query: {
-      products: (): GQL.IQuery['products'] => ([])
+      products
     }
   },
   typeDefs: gql`
